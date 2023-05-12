3f7805bf020f480f8a6cd028f7115356dc380f8e
FanOne <294350394@qq.com>
2023-05-12 14:43:45 +0800

    feat:upload

---
+++ src/components/ProductDetailUploadImg.tsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import {   Upload, Form, Input, Modal, Radio } from 'antd';
+import { InboxOutlined, UploadOutlined } from '@ant-design/icons';
+
+interface Values {
+    title: string;
+    description: string;
+    modifier: string;
+}
+
+interface CollectionCreateFormProps {
+    open: boolean;
+    onCreate: (values: Values) => void;
+    onCancel: () => void;
+}
+
+const normFile = (e: any) => {
+    console.log('Upload event:', e);
+    if (Array.isArray(e)) {
+        return e;
+    }
+    return e?.fileList;
+};
+
+const CollectionCreateForm: React.FC<CollectionCreateFormProps> = ({open, onCreate, onCancel,}) => {
+    const [form] = Form.useForm();
+    
+    return (
+        <Modal
+            open={open}
+            title="Create a new collection"
+            okText="Create"
+            cancelText="Cancel"
+            onCancel={onCancel}
+            onOk={() => {
+                form
+                    .validateFields()
+                    .then((values) => {
+                        form.resetFields();
+                        onCreate(values);
+                    })
+                    .catch((info) => {
+                        console.log('Validate Failed:', info);
+                    });
+            }}
+        >
+            <Form
+                form={form}
+                layout="vertical"
+                name="form_in_modal"
+                initialValues={{ modifier: 'public' }}
+            >
+                
+                <Form.Item
+                    name="title"
+                    label="Title"
+                    rules={[{ required: true, message: 'Please input the title of collection!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                
+                <Form.Item name="description" label="Description">
+                    <Input type="textarea" />
+                </Form.Item>
+    
+                <Form.Item label="Dragger">
+                    <Form.Item name="dragger" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
+                        <Upload.Dragger name="files" action="/upload.do">
+                            <p className="ant-upload-drag-icon">
+                                <InboxOutlined />
+                            </p>
+                            <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                            <p className="ant-upload-hint">Support for a single or bulk upload.</p>
+                        </Upload.Dragger>
+                    </Form.Item>
+                </Form.Item>
+                
+                <Form.Item name="modifier" className="collection-create-form_last-form-item">
+                    <Radio.Group>
+                        <Radio value="public">Public</Radio>
+                        <Radio value="private">Private</Radio>
+                    </Radio.Group>
+                </Form.Item>
+                
+                
+                
+            </Form>
+        </Modal>
+    );
+};
+
+
+export default CollectionCreateForm;
